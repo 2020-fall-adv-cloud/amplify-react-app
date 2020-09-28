18fe51a4aabf0eec7206b907b021479c2fd87a47
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-09-28 13:28:55 -0500

    Wrote the initial /coins route code...
    And went on and on about some BS JS shorthand syntax...

--- amplify/backend/function/cryptofunction/src/app.js
+++ amplify/backend/function/cryptofunction/src/app.js
@@ -30,6 +30,30 @@ app.use(function(req, res, next) {
  * Example get method *
  **********************/
 
+app.get(
+   '/coins'
+   , (req, res) => {
+     
+    const coins = [
+      {name: 'Bitcoin', symbol: 'BTC', price_usd: '10000' } 
+      , {name: 'Ethereum', symbol: 'ETH', price_usd: '400' } 
+      , {name: 'Litecoin', symbol: 'LTC', price_usd: '150' } 
+    ];
+  
+    res.json({
+      coins
+
+      // I believe 'coins' above to be shorthand for...
+      // coins: coins
+      //
+      // So that I could use a different property name, such as
+      // myCoins: coins
+    });
+  }
+);
+
+
+
 app.get('/item', function(req, res) {
   // Add your code here
   res.json({success: 'get call succeed!', url: req.url});
