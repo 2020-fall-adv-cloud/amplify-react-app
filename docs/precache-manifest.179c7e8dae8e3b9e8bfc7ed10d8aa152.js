262ae1dfa852fed33f6352918c00116c26b020b8
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-10-06 18:38:46 -0500

    Attempting GitHub pages publish
    npm run build
    
    move build docs

---
+++ docs/precache-manifest.179c7e8dae8e3b9e8bfc7ed10d8aa152.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8757f879f55bb2d153541b7e988ac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df7238076ccbacb8dbaa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9c9980fd528769b1dd3",
+    "url": "/static/js/2.6e969a72.chunk.js"
+  },
+  {
+    "revision": "3d4d9872e604682782346535cc277e64",
+    "url": "/static/js/2.6e969a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7238076ccbacb8dbaa",
+    "url": "/static/js/main.d102d55e.chunk.js"
+  },
+  {
+    "revision": "4cdbbf82b2af7937937a",
+    "url": "/static/js/runtime-main.06abdcaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
