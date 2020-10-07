37c0e9084f5613ec6b28c3df3993c0905711ff86
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-10-07 13:44:54 -0500

    My GitHubBornOn component placeholder...

---
+++ src/GitHubBornOn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GitHubBornOn = () => {
+
+    return (
+        <h3>
+            tsteelematc - 20160812asdfasdfasdfasdfasdfasdf
+        </h3>
+    );
+}
+
+export default GitHubBornOn;
