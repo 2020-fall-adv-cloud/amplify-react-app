d145b64d984872817745da2ac99eccb541d915f6
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-10-07 14:08:59 -0500

    Completed my #project-03...

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
+import { API } from 'aws-amplify';
 
 const GitHubBornOn = () => {
 
+    const [bornOn, setBornOn] = useState({ username: 'unknown', date: 'unknown' });
+
+    const fetchBornOn = async () => {
+      const data = await API.get('cryptoapi', '/born');
+      setBornOn({
+        username: data.borninfo.login
+        , date: data.borninfo.created_at
+      });
+    }
+  
+    useEffect(
+      () => {
+        fetchBornOn();
+      }
+      , []
+    );
+
     return (
         <h3>
-            tsteelematc - 20160812asdfasdfasdfasdfasdfasdf
+            { bornOn.username } - { bornOn.date }
         </h3>
     );
 }
